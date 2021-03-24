4fc01f82c5dc244569610c2593a74be1b6c96525
CandyOre <834657460@qq.com>
2021-03-24 14:42:25 +0800

    str_to_double update

---
+++ str_to_double.h
@@ -0,0 +1,13 @@
+#include <string>
+
+//A string representing a float number to a double number
+double func ( std::string str ) {
+    int i;
+    for ( i = 0; i < str.size(); i++ ) if ( str[i] == '.' ) break;
+    if ( i == str.size() ) return 0;
+    int a = 0;
+    for ( int j = 0; j < i; j++ ) a = a * 10 + ( str[j] - '0' );
+    double b = 0;
+    for ( int j = str.size() - 1; j > i; j--) b = b / 10 + 0.1 * ( str[j] - '0' );
+    return a + b;
+}
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include "str_to_double.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str = "123.45";
+    cout << func ( str ); 
+    return 0;
+}
